e7ff8c41850cad039a0fdc2c30db08ccfa7f9631
Felipe Thomé <119542554+felipetn1989@users.noreply.github.com>
2023-02-04 14:42:59 -0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,15 @@
 notify.addEventListener("click", submit);
 
-function isEmail(ifmail) {
-  let mailformat =
-    /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+/* Regex to test if the string provided on the mail form is a valid e-mail address */
 
-  if (ifmail.value.match(mailformat)) {
-    return true;
-  } else {
-    return false;
-  }
-}
+let mailformat = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+
+/* Function do display the error message and do the appropriate styling changes if the e-mail is not valid or no e-mail is provided by the user */
 
 function submit(x) {
   x.preventDefault();
 
-  if (ifmail.value.length == 0 || isEmail(ifmail) == false) {
+  if (ifmail.value.length == 0 || !mailformat.test(ifmail.value)) {
     ifmail.style.borderColor = "var(--Light-Red)";
     errorMsg.style.display = "block";
   } else {
